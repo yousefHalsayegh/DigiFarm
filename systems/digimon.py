9ec4c0b13827001fb4231808f857e315a1a0a49c
yousefHalsayegh <96050534+yousefHalsayegh@users.noreply.github.com>
2024-10-05 13:36:15 +0300

    Added gitignore

--- systems/digimon.py
+++ systems/digimon.py
@@ -6,15 +6,16 @@ class Digimon:
         self.type = data["Type"]
         self.family = data["Family"]
         self.core = data["Core"]
-        self.rect = pg.Rect(200, 100, 10, 30)
         self.pos = (200,100)
         self.size = (10, 30)
         self.colour = (0,0,0)
         self.speed = 1
+        self.sprite = None
+        self.rect =  self.sprite.get_rect()
     
     
     def move(self, b):
         self.rect.left += random.randint(0,10) * self.speed * random.randint(-1,1) 
         self.rect.top += random.randint(0,10) * self.speed * random.randint(-1,1)
         
-        pg.draw.rect(b, self.colour, self.rect)
+        
